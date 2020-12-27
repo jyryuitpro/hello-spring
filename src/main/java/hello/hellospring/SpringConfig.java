e8b75e0da152a74be29b77d5573b4c94cde99015
jyryuitpro <jy.ryu.itpro@gmail.com>
2020-12-27 20:11:34 +0900

    스프링 JdbcTemplate

--- src/main/java/hello/hellospring/SpringConfig.java
+++ src/main/java/hello/hellospring/SpringConfig.java
@@ -1,8 +1,7 @@
 package hello.hellospring;
 
-import hello.hellospring.domain.repository.JdbcMemberRepository;
+import hello.hellospring.domain.repository.JdbcTemplateMemberRepository;
 import hello.hellospring.domain.repository.MemberRepository;
-import hello.hellospring.domain.repository.MemoryMemberRepository;
 import hello.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
@@ -28,6 +27,7 @@ public class SpringConfig {
     @Bean
     public MemberRepository memberRepository() {
 //        return new MemoryMemberRepository();
-        return new JdbcMemberRepository(dataSource);
+//        return new JdbcMemberRepository(dataSource);
+        return new JdbcTemplateMemberRepository(dataSource);
     }
 }
